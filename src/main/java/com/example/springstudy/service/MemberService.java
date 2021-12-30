22ce1867cda4fd00de7a3126543656f77d1c1735
Lee Hoseop <ghtjq3909@naver.com>
2021-12-30 17:22:30 +0900

    회원 서비스 개발

---
+++ src/main/java/com/example/springstudy/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.example.springstudy.service;
+
+import com.example.springstudy.domain.Member;
+import com.example.springstudy.repository.MemberRepository;
+import com.example.springstudy.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public long join(Member member) {
+        // 중복 검사
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}

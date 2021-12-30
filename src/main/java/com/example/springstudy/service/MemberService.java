3dd4edd60db0ec9c11495b7bf4375411a7f92004
Lee Hoseop <ghtjq3909@naver.com>
2021-12-30 22:33:59 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/com/example/springstudy/service/MemberService.java
+++ src/main/java/com/example/springstudy/service/MemberService.java
@@ -2,15 +2,19 @@ package com.example.springstudy.service;
 
 import com.example.springstudy.domain.Member;
 import com.example.springstudy.repository.MemberRepository;
-import com.example.springstudy.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

c7ef6b1a1cb44f2eaef62189f88f7788a2e9cd84
Lee Hoseop <ghtjq3909@naver.com>
2022-01-01 20:00:05 +0900

    스프링 JdbcTemplate

--- src/main/java/com/example/springstudy/SpringConfig.java
+++ src/main/java/com/example/springstudy/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.example.springstudy;
 
 import com.example.springstudy.repository.JdbcMemberRepository;
+import com.example.springstudy.repository.JdbcTemplateMemberRepository;
 import com.example.springstudy.repository.MemberRepository;
 import com.example.springstudy.repository.MemoryMemberRepository;
 import com.example.springstudy.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
